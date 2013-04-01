e1666475d46473e56d53cfe908ccca7b56b5f794
Rob Worsnop <robert.worsnop@altisource.com>
2013-04-01 13:27:42 -0400

    Got master putting messages on queue

--- src/test/java/com/opencredo/sandbox/gawain/springbatch/remote/chunking/RemoteChunkingIntegrationTest.java
+++ src/test/java/com/opencredo/sandbox/gawain/springbatch/remote/chunking/RemoteChunkingIntegrationTest.java
@@ -11,6 +11,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import org.springframework.batch.core.BatchStatus;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.core.MessagingOperations;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.util.FileSystemUtils;
 
@@ -19,28 +24,51 @@ import org.springframework.util.FileSystemUtils;
 public class RemoteChunkingIntegrationTest {
 
 	private static final Log logger = LogFactory.getLog(RemoteChunkingIntegrationTest.class);
+
+    @Test
+    public void mytest(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/master/master-integration-context.xml");
+        MessagingOperations ops = context.getBean(MessagingOperations.class);
+        MessageChannel channel1 = (MessageChannel)context.getBean("toRabbit");
+        MessageChannel channel3 = (MessageChannel)context.getBean("fromRabbit");
+        MessageChannel channel2 = (MessageChannel)context.getBean("requests");
+        while (ops == null){
+            channel1.send(MessageBuilder.withPayload("hello").build());
+            channel3.send(MessageBuilder.withPayload("hello").build());
+
+            //           ops.send(MessageBuilder.withPayload("hello").build());
+        }
+
+    }
 	
 	@Test
 	public void testRemoteChunkingOnMultipleSlavesShouldLoadBalanceAndComplete() throws Exception {
 
-		final BrokerContext broker = (BrokerContext) new BrokerContext("classpath:/broker/broker-context.xml").start();
+
+ //       final BrokerContext broker = (BrokerContext) new BrokerContext("classpath:/broker/broker-context.xml").start();
 		final MasterBatchContext masterBatchContext = new MasterBatchContext("testjob", "classpath:/master/master-batch-context.xml");
-		final SlaveContext slaveContext1 = new SlaveContext("classpath:/slave/slave1-batch-context.xml");
-		final SlaveContext slaveContext2 = new SlaveContext("classpath:/slave/slave2-batch-context.xml");
+//		final SlaveContext slaveContext1 = new SlaveContext("classpath:/slave/slave1-batch-context.xml");
+//		final SlaveContext slaveContext2 = new SlaveContext("classpath:/slave/slave2-batch-context.xml");
 
 		masterBatchContext.start();
-		slaveContext1.start();
-		slaveContext2.start();
+
+ //       Thread.sleep(700000);
+
+//		slaveContext1.start();
+//		slaveContext2.start();
+
+
 		
 		BatchJobTestHelper.waitForJobTopComplete(masterBatchContext);
 
 		final BatchStatus batchStatus = masterBatchContext.getBatchStatus();
 		logger.info("job finished with status: " + batchStatus);
 		Assert.assertEquals("Batch Job Status", BatchStatus.COMPLETED, batchStatus);
-		logger.info("slave 1 chunks written: " + slaveContext1.writtenCount() );
-		logger.info("slave 2 chunks written: " + slaveContext2.writtenCount() );
-		Assert.assertEquals("slave chunks written", 5, slaveContext1.writtenCount() ); 
-		Assert.assertEquals("slave chunks written", 5, slaveContext2.writtenCount()); 
+//		logger.info("slave 1 chunks written: " + slaveContext1.writtenCount() );
+//		logger.info("slave 2 chunks written: " + slaveContext2.writtenCount());
+//		Assert.assertEquals("slave chunks written", 5, slaveContext1.writtenCount());
+//		Assert.assertEquals("slave chunks written", 5, slaveContext2.writtenCount());
+
 
 	}	
 	
